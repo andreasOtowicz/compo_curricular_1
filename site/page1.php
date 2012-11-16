88aba414c0b9c58cefcedd4a892fdf788b87c60f
andreas <andreas_otowicz00@hotmail.com>
2012-11-16 20:05:08 -0300

    inicio da pagina 1

--- site/page1.php
+++ site/page1.php
@@ -1,1 +1,19 @@
-   
+   <!DOCTYPE html>
+<html lang="pt">
+	
+	<head>
+		<title>Construct</title>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+	</head>
+	
+	<body>
+	
+	shaudhasiufhuiashfiudsih
+	
+	
+	</body>
+	
+	
+	
+	</html>
+	
