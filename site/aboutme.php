064192e532d9f1074741595e73749d4377044ed5
andreas <andreas_otowicz00@hotmail.com>
2012-11-19 00:11:00 -0300

    Adicionando pagina aboutme

---
+++ site/aboutme.php
@@ -0,0 +1,135 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Trabalho pagina</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descri? do site trabalho Pagina">
+    <meta name="author" content="Andreas Otowicz - andreas_otowicz00@hotmail.com">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+    
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          
+          <a class="brand" href="#">Meu site</a>
+          <div class="nav-collapse collapse">
+            
+            <ul class="nav">
+              <li class="active"><a href="page1.php">AboutMe</a></li>
+              <li><a href="page1.php">Pagina 1</a></li>
+              <li><a href="page2.php">Pagina 2</a></li>
+			  <li><a href="contact.php">Contato</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row-fluid">
+	  
+        <div class="span3">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li class="nav-header">Sidebar</li>
+              <li class="active"><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
+       
+
+	   <div class="span9">
+          <div class="hero-unit">
+            <h1>Sobre Mim</h1>
+			
+			<!DOCTYPE html>
+
+<html>
+		<head>
+		<meta charset="utf-8"/>
+		<title>Sobre Mim </title>
+		</head>
+	<body>
+		<h2>Apresentação</h2>
+	<p>Eu, Andreas Miguel de Oliveira Otowicz, tenho 24 anos. Moro em Xanxerê e trabalho de dia em uma lotérica, à noite estudo na UNOESC Campus de Xanxerê e estou fazendo o curso de Bombeiro Comunitário aos sábados.
+	Sou solteiro e moro com os meus pais, Alexandre e Cleuza. 
+	Tenho dois irmãos, Gustavo e Camila, sendo que minha irmã não mora mais conosco, e sim em Porto Belo, no litoral catarinense.</p>
+
+		<h2>Conhecimentos</h2>
+	<p>Possuo conhecimento na área de desenvolvimento de software, como caixa, segurança do trabalho, recebimento e expedição de mercadorias, combate a incêndios 
+	e atendimento pré-hospitalar.</p>
+
+		<h2>Objetivos pessoais</h2>
+	<p>Um dos maiores objetivos em curto prazo é se tornar um bombeiro comunitário e também me formar no meu curso na UNOESC. 
+	E consequentemente atuar na área de TI em uma empresa, ou senão se conseguir passar no concurso para bombeiros militar 
+	ou para bombeiro comunitário profissional, atuar nessa área.</p>
+
+		<h2>Contatos</h2>
+	<p><b>Telefone celular:</b> 4999202923.</p>
+	<p><b>Trabalho no período do dia:</b> 4934330866.</p>  
+	
+	</body>
+</html>
+         
+			
+
+  
+            </div><!--/span-->
+          </div><!--/row-->
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2012</p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
